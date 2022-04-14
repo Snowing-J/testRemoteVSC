1ce07a9aa39e28446d08dca63387be6e37f27cb0
mayonggui <patrick-m@foxmail.com>
2022-04-14 16:21:05 +0800

    a

--- main.cpp
+++ main.cpp
@@ -4,7 +4,9 @@ int main() {
     std::cout << "Hello, World!" << std::endl;
     int a = 10;
     int b = 2;
+    int c = 3;
     std::cout << "get a:" << a << std::endl;
     std::cout << "get b:" << b << std::endl;
+    std::cout << "get c:" << c << std::endl;
     return 0;
 }
