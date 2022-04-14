baccd266ab2265a668825c596f0bc5256c88a3fa
mayonggui <patrick-m@foxmail.com>
2022-04-14 14:03:31 +0800

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/testRemoteVSC.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/testRemoteVSC.dir/main.cpp.o"
+  "testRemoteVSC.pdb"
+  "testRemoteVSC"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/testRemoteVSC.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
